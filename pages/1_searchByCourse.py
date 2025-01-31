3f6223d505fe385b2ec8b0727bfcb2cb97628d65
Yavuz Dev <yavuzdev@Yavuzs-MBP.hitronhub.home>
2025-01-31 01:15:39 -0500

    Added requirements file

--- pages/1_searchByCourse.py
+++ pages/1_searchByCourse.py
@@ -11,24 +11,21 @@ def academic_term_sort_key(term):
     adjusted_year = year if season == 'F' else year - 1
     return adjusted_year, season_order[season]
 
-# def sort_df_by_term(df):
-#     # Sort the DataFrame using the custom key
-#     df['SortKey'] = df['TermName'].apply(academic_term_sort_key)
-#     df = df.sort_values('SortKey').drop(columns=['SortKey'])
-#
-#     # Reset the index after sorting
-#     df.reset_index(drop=True, inplace=True)
-#     return df
+def sort_df_by_term(df):
+    # Sort the DataFrame using the custom key
+    df['SortKey'] = df['TermName'].apply(academic_term_sort_key)
+    df = df.sort_values('SortKey').drop(columns=['SortKey'])
+
+    # Reset the index after sorting
+    df.reset_index(drop=True, inplace=True)
+    return df
 
 
-# Dynamically resolve the CSV path
 DATA_FILE = os.path.join(os.path.dirname(os.path.dirname(__file__)), "selected_classes_byProf.csv")
 
 st.set_page_config(layout="wide")
 
-st.title("McGill Professor Difficulty Dashboard")
-
-# Read and display the CSV
+st.title("ProfSight McGill: Course Dashboard")
 
 data = pd.read_csv(DATA_FILE)
 
@@ -70,9 +67,8 @@ if course_name == "":
             st.metric(label="Terms Represented:",
                   value=data.groupby('TermName').size().reset_index(name='Count').shape[0])
 
-        summary = data.groupby(['SubjectCode', 'TermName'])['ClassAveNum'].mean().reset_index()
+        summary = data.groupby(['TermName', 'SubjectCode'])['ClassAveNum'].mean().reset_index()
 
-        # Rename columns for clarity if needed
         summary.rename(columns={'ClassAveNum': 'MeanGPA'}, inplace=True)
 
         summary['TermName'] = pd.Categorical(
@@ -83,11 +79,18 @@ if course_name == "":
 
         sorted_summary = summary.sort_values('TermName')
 
-        # Display or use the summarized DataFrame
-        grades_by_time_bar = px.line(sorted_summary, x="TermName", y="MeanGPA", color="SubjectCode",
-                                     color_discrete_sequence=px.colors.sequential.amp_r)
+        #GRADES OVER TIME BAR CHART
 
-        st.write(sorted_summary)
+        st.divider()
+        
+        st.write("**Departmental Mean Class Average by Year:**")
+        courses_bar = st.multiselect(label="Choose department(s):", options=data['SubjectCode'].unique(), 
+                       default=data['SubjectCode'].unique()[0])
+        
+        grades_by_time_bar = px.line(sorted_summary[sorted_summary["SubjectCode"].isin(courses_bar)], x="TermName", y="MeanGPA", color="SubjectCode",
+                                     color_discrete_sequence=px.colors.sequential.amp)
+
+        #st.write(sorted_summary)
         st.plotly_chart(grades_by_time_bar)
 
 
@@ -103,6 +106,7 @@ else:
 
     sorted_courseOverTime = courseOverTime.sort_values(by="ClassAveNum")
     median_grade = sorted_courseOverTime["ClassAveLetter"].iloc[sorted_courseOverTime.shape[0] // 2]
+    std_dev_grade = round(sorted_courseOverTime["ClassAveNum"].std(), 3)
 
     #prof_name = st.text_input("Enter Professor name: (e.g. 'Asgharian, Masoud')")
 
@@ -115,8 +119,6 @@ else:
 
 
     #Computing the median grade of a course
-
-
     with col1:
 
         col11, col12 = st.columns(2)
@@ -124,6 +126,9 @@ else:
         with col11:
 
             st.metric(label = "Median Grade", value=median_grade)
+        
+        with col12:
+            st.metric(label = "Std. Deviation (GPA)", value = std_dev_grade)
 
         st.plotly_chart(fig, theme=None, use_container_width=True)
 
@@ -141,36 +146,26 @@ else:
     #TODO: Fix the sort feature of the grades by prof chart
     sortedCourseAveByProf = courseAveByProf.sort_values(by="ClassAveNum", ascending=False)
 
-    # st.bar_chart(sortedCourseAveByProf, x="Instructor_1", y="ClassAveNum", x_label="Professor",
-    #              y_label="Average GPA Points", color="Count")
-
-    #st.bar_chart(sortedCourseAveByProf.set_index('Instructor_1')['ClassAveNum'])
-
     st.write(sortedCourseAveByProf)
 
     with col2:
-        # st.write("Course Average by Professor")
-        # st.bar_chart(sortedCourseAveByProf, x="Instructor_1", y="ClassAveNum", x_label="Professor",
-        #              y_label="Average GPA Points", color="Count")
-
-        #df = px.data.gapminder().query("continent == 'Oceania'")
         fig = px.bar(sortedCourseAveByProf, x='Instructor_1', y='ClassAveNum',
                      hover_data=['Instructor_1', 'ClassAveNum'], color='Count',
                      labels={'Instructor_1': 'Professor'}, height=400,
-                     color_continuous_scale=['#8B0000', '#A23232', '#B86464', '#CF9696', '#E5C8C8']
-)
-
-        #fig.show()
+                     color_continuous_scale=['#8B0000', '#A23232', '#B86464', '#CF9696', '#E5C8C8'])
 
         st.plotly_chart(fig)
-
-
 
         st.divider()
 
         st.write("Course Average over Time")
-        st.bar_chart(courseOverTime, x="TermName", y="ClassAveNum", x_label="Term", y_label="Average GPA Points",
+
+        st.bar_chart(sort_df_by_term(courseOverTime).sort_values("TermName"), x="TermName", y="ClassAveNum", x_label="Term", y_label="Average GPA Points",
                      color="#8B0000")
+
+        st.write(courseOverTime.sort_values("TermName"))
+
+        st.write(sort_df_by_term(courseOverTime))
 
     #Build out courses page first
 
